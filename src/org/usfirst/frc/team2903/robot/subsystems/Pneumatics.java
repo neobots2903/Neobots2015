f03c0cf54b03f9135b0c91162463d58a20450b09
alliecatstanz34 <astansberry@hotmail.com>
2015-02-07 14:12:34 -0800

    added subsystems

---
+++ src/org/usfirst/frc/team2903/robot/subsystems/Pneumatics.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team2903.robot.subsystems;
+
+public class Pneumatics extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
