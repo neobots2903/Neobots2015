e79d452e7ba99fa02334a056356ee976f11e5368
groot314 <groot314@gmail.com>
2015-02-07 15:43:45 -0800

    Added motors to RobotMap

--- src/org/usfirst/frc/team2903/robot/RobotMap.java
+++ src/org/usfirst/frc/team2903/robot/RobotMap.java
@@ -6,13 +6,11 @@ package org.usfirst.frc.team2903.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+
+    public static int HDriveM1 = 1;
+    public static int HDriveM2 = 2;
+    public static int HDriveM3 = 3;
+    public static int HDriveM4 = 4;
+    public static int HDriveM5 = 5;
+    public static int relevatorM = 6;
 }
