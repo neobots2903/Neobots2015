c61fda740d623fe21d5556d5faa7f2cdfe8a3c8a
nemfreed <nemfreed@gmail.com>
2015-02-07 16:14:05 -0800

    started on drive subsystems

--- src/org/usfirst/frc/team2903/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team2903/robot/subsystems/Drive.java
@@ -1,14 +1,18 @@
 package org.usfirst.frc.team2903.robot.subsystems;
 
+import org.usfirst.frc.team2903.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drive extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	public Talon leftFrontMotor1 = new Talon(RobotMap.HDriveM2);
+	public Talon rightFrontMotor1 = new Talon(RobotMap.HDriveM3);
+	public Talon leftBackMotor1 = new Talon(RobotMap.HDriveM1);
+	public Talon rightBackMotor1 = new Talon(RobotMap.HDriveM4);
+	public Talon middleMotor1 = new Talon(RobotMap.HDriveM5);
+	
+	
 }
