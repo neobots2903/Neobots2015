12bf40672479e22634e7c83257d2081f5edebff8
buddybu <listbuddy@frontier.com>
2015-02-15 14:57:24 -0800

    Committing for Code

--- src/org/usfirst/frc/team2903/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team2903/robot/subsystems/Pneumatics.java
@@ -1,31 +1,83 @@
 package org.usfirst.frc.team2903.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Pneumatics extends Subsystem {
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
-	public Solenoid mySol1 = new Solenoid(1); 
-	public void mySol1(){}				/* This makes the variable called
-												 mySol1 a Solenoid from port
-												 1 and allows us to activate 
-												 it*/
-	public Solenoid mySol2 = new Solenoid(2); 
-												/* This makes the variable called
-												 mySol2 a Solenoid from port
-												 2 and allows us to activate 
-												 it*/
-	public DoubleSolenoid myDoubleSol = new DoubleSolenoid(1, 2); /* This allow
-																	 us to use
-																	 the
-																	 Solenoids
-																	 together.*/
+	
+	final int RIGHTSOLOPEN = 1;
+	final int RIGHTSOLCLOSE = 2;
+	final int LEFTSOLOPEN = 6;
+	final int LEFTSOLCLOSE = 7;
 
-	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
-
+	public Solenoid rightSolOpen = new Solenoid(RIGHTSOLOPEN); 
+	public Solenoid rightSolClose = new Solenoid(RIGHTSOLOPEN);
+	public Solenoid leftSolOpen = new Solenoid(LEFTSOLOPEN);
+	public Solenoid leftSolClose = new Solenoid(LEFTSOLCLOSE);
+	public DoubleSolenoid doubleSolOpen = new DoubleSolenoid(RIGHTSOLOPEN, LEFTSOLOPEN);
+	public boolean isOpen = false; 
+	
+		
+	public void openarms()
+	{
+		doubleSolOpen.set(Value.kForward);
 	}
-}
+	
+	public void closearms()
+	{
+		doubleSolOpen.set(Value.kReverse);
+	}
+	
+	public void leftarmopen()
+	{
+		leftSolOpen.set(true);
+		leftSolClose.set(false);
+	}
+	
+	public void leftarmclose()
+	{
+		leftSolOpen.set(false);
+		leftSolClose.set(true);
+	}
+	
+	public void rightarmopen()
+	{
+		rightSolOpen.set(true);
+		rightSolOpen.set(false);
+	}
+	
+	public void rightarmclose()
+	{
+		rightSolOpen.set(false);
+		rightSolOpen.set(true);
+	}
+	
+	
+	public void armReset()
+	{
+		openarms();
+		doubleSolOpen.set(Value.kOff);
+	}
+	
+	public void armsOpenClose()
+	{
+		if (isOpen)
+		{
+			closearms();
+			isOpen = false;
+		}
+		else
+		{
+			openarms();
+			isOpen = true; 
+		}
+	}
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	}
