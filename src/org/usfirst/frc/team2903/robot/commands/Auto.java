8ec76243b0ac4eb4298bf2fe60c7ac193f7515fa
Barschaw <codybarschaw@yahoo.com>
2015-02-27 18:44:46 -0800

    Autonomous program for the robotn

--- src/org/usfirst/frc/team2903/robot/commands/Auto.java
+++ src/org/usfirst/frc/team2903/robot/commands/Auto.java
@@ -1,32 +1,77 @@
 package org.usfirst.frc.team2903.robot.commands;
 
 import org.usfirst.frc.team2903.robot.Robot;
+import org.usfirst.frc.team2903.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Auto extends Command {
 
-	    public Auto() {
-	        requires(Robot.driveSubsystem);
-	        requires(Robot.elevatorSubsystem);
-	        requires(Robot.pneumaticsSubsystem);
-	    }
+	public Talon leftFrontMotor1 = new Talon(RobotMap.HDriveM2);
+	public Talon rightFrontMotor1 = new Talon(RobotMap.HDriveM3);
+	public Talon leftBackMotor1 = new Talon(RobotMap.HDriveM1);
+	public Talon rightBackMotor1 = new Talon(RobotMap.HDriveM4);
+	public Talon middleMotor1 = new Talon(RobotMap.HDriveM5);
+	public RobotDrive hDrive = new RobotDrive(1, 2, 3, 4);
 
-	    protected void initialize() {
-	    }
+	public void forward() {
+		long startTime = System.currentTimeMillis();
+		long stopTime = startTime;
 
-	    protected void execute() {
-	    }
+		rightFrontMotor1.set(1);
+		leftFrontMotor1.set(1);
+		leftBackMotor1.set(1);
+		rightBackMotor1.set(1);
 
-	    protected boolean isFinished() {
-	        return false;
-	    }
+		while (stopTime <= (startTime + 2000)) {
 
-	    protected void end() {
-	    }
+			stopTime = System.currentTimeMillis();
 
-	    protected void interrupted() {
-	    }
+		}
+
+		rightFrontMotor1.set(0);
+		leftFrontMotor1.set(0);
+		leftBackMotor1.set(0);
+		rightBackMotor1.set(0);
 	}
 
+	public void drive(double xAxis, double yAxis, double mid) {
+		hDrive.arcadeDrive(xAxis, yAxis);
+		middleMotor1.set(mid);
+	}
 
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+}
