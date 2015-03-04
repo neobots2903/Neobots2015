3ae21b7c069b66a14845ee6016c6088bb398dca2
BlakeCoulter <BlakeCoulter8@hotmail.com>
2015-03-04 10:24:34 -0800

    The start to the gyro code. So sorry I thought I posted it yesterday (the 3 of May).

---
+++ src/org/usfirst/frc/team2903/robot/subsystems/Gyro2903.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team2903.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Gyro;
+import org.usfirst.frc.team2903.robot.OI;
+
+public class Gyro2903 {
+	public void Gyro() {
+
+		Gyro gyro = new Gyro(1);
+		gyro.reset();
+			
+		double angle = gyro.getAngle();
+		double	x = OI.Joy2.getX();
+		double CAngle = gyro.getAngle();
+		
+		if(x == 0){
+		
+			if(angle == 0){
+				
+			}
+			else{
+			//turn to 0
+				}
+			}
+		else{
+		
+		}
+		}
+} 
