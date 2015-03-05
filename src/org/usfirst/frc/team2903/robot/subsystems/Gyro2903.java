ee186fb3328ccfd9c3e6c9dd4ec3533f8a62b85d
alliecatstanz34 <astansberry@hotmail.com>
2015-03-05 08:11:34 -0800

    problem with commit

--- src/org/usfirst/frc/team2903/robot/subsystems/Gyro2903.java
+++ src/org/usfirst/frc/team2903/robot/subsystems/Gyro2903.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team2903.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Gyro;
 import org.usfirst.frc.team2903.robot.OI;
-
+//dhjgf
 public class Gyro2903 {
 	public void Gyro() {
 
